976dd9208bd734e8cff9e4b05b4f12c58a90f011
Isaac Amaro <isaac.amaro03@gmail.com>
2021-08-02 00:50:06 -0300

    Added empty list illustration

--- src/components/Root/Main/AlarmList/AlarmList.js
+++ src/components/Root/Main/AlarmList/AlarmList.js
@@ -4,9 +4,15 @@ import { AlarmItem } from "./AlarmItem";
 
 const useStyles = makeStyles({
   root: {
+    display: "flex",
+    flexFlow: "column",
     flex: 1,
     marginBottom: "1rem",
     overflow: "auto",
+
+    "& > img": {
+      margin: "auto 0",
+    },
   },
   wrapper: {
     display: "flex",
@@ -19,13 +25,21 @@ const useStyles = makeStyles({
 const AlarmList = ({ alarms }) => {
   const classes = useStyles();
 
+  const EmptyListIllustration = () => {
+    return <img src="./web-alarm/clock_illustration.svg" />;
+  };
+
   return (
     <div className={classes.root}>
-      <div className={classes.wrapper}>
-        {alarms.map((alarm) => (
-          <AlarmItem alarmData={alarm} />
-        ))}
-      </div>
+      {alarms.length ? (
+        <div className={classes.wrapper}>
+          {alarms.map((alarm) => (
+            <AlarmItem alarmData={alarm} />
+          ))}
+        </div>
+      ) : (
+        <EmptyListIllustration />
+      )}
     </div>
   );
 };
